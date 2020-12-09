a10ae43b6eeb52147dcf47af9679f2a7bd8570d8
Konstantinos Alexiou <konalexiou@gmail.com>
2020-12-09 20:58:56 +0200

    minor change

--- PlacesPicker/Classes/PlacePicker.swift
+++ PlacesPicker/Classes/PlacePicker.swift
@@ -14,7 +14,7 @@ public class PlacePicker {
     static var googleMapsKey: String?
     static var googleGeocodeKey: String?
 
-    public static func configure(googleMapsAPIKey: String, placesAPIKey: String) {
+    public static func configure(googleMapsAPIKey: String, placesAPIKey: String, googleGeocodeAPIKey: String) {
         GMSServices.provideAPIKey(googleMapsAPIKey)
         GMSPlacesClient.provideAPIKey(placesAPIKey)
         googleMapsKey = googleMapsAPIKey
